76a510af57a678d219a143491b1005f1b492f777
Sami Tabet <salph.tabet@gmail.com>
2024-11-25 09:24:00 +0100

    fix tag reporting

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ datadog_forwarder:
     app_key: my_app_key
 
 """
+
 from __future__ import annotations
 
 import time
@@ -24,8 +25,8 @@ from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.metrics_api import MetricsApi
 from datadog_api_client.v2.model.metric_payload import MetricPayload
 from datadog_api_client.v2.model.metric_series import MetricSeries
-from datadog_api_client.v2.model.metric_point import  MetricPoint
-from datadog_api_client.v2.model.intake_payload_accepted import  IntakePayloadAccepted
+from datadog_api_client.v2.model.metric_point import MetricPoint
+from datadog_api_client.v2.model.intake_payload_accepted import IntakePayloadAccepted
 
 from collections import namedtuple, defaultdict
 
@@ -44,21 +45,26 @@ from homeassistant.helpers import state as state_helper
 
 _LOGGER = logging.getLogger(__name__)
 
-MetricId = namedtuple('MetricId', ["name", "tags"])
-Value = namedtuple('Value', ["id", "timestamp", "value"])
+MetricId = namedtuple("MetricId", ["name", "tags", "unit"])
+Value = namedtuple("Value", ["id", "timestamp", "value"])
+
 
 def send_values(api: MetricsApi, values: List[Value]) -> IntakePayloadAccepted:
 
     by_name: Dict[MetricId, List[MetricPoint]] = defaultdict(list)
 
     for value in values:
-        by_name[value.id].append(MetricPoint(timestamp=value.timestamp, value=value.value))
+        by_name[value.id].append(
+            MetricPoint(timestamp=value.timestamp, value=value.value)
+        )
 
     series: List[MetricSeries] = []
 
     for id, points in by_name.items():
         points.sort(key=lambda p: p.timestamp)
-        serie = MetricSeries(metric=id.name, points=points, tags=[*id.tags])
+        serie = MetricSeries(
+            metric=id.name, points=points, tags=[*id.tags], unit=id.unit
+        )
         series.append(serie)
 
     body = MetricPayload(series)
@@ -82,7 +88,9 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_API_KEY): cv.string,
                 vol.Required(CONF_APP_KEY): cv.string,
                 vol.Optional(CONF_FLUSH_PERIOD_SEC, default=60): int,
-                vol.Optional(CONF_PREFIX, default="home_assistant.datadog_forwarder"): cv.string,
+                vol.Optional(
+                    CONF_PREFIX, default="home_assistant.datadog_forwarder"
+                ): cv.string,
                 vol.Optional(CONF_TAGS, default=""): cv.string,
             }
         )
@@ -90,12 +98,14 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
+
 def ts() -> int:
     return int(time.time())
 
+
 class ValueBuffer:
-    def __init__(self,  api: MetricsApi, flush_period_sec: int):
-        self._b : List[Value] = []
+    def __init__(self, api: MetricsApi, flush_period_sec: int):
+        self._b: List[Value] = []
         self._last_send: int = ts()
         self._flush_period_sec = flush_period_sec
         self._api = api
@@ -106,19 +116,20 @@ class ValueBuffer:
         if ts() - self._last_send > self._flush_period_sec:
             res = send_values(self._api, self._b)
             if res.errors:
-                _LOGGER.error("An error occurred sending %d points: %s", len(self._b), ",".join(res.errors))
+                _LOGGER.error(
+                    "An error occurred sending %d points: %s",
+                    len(self._b),
+                    ",".join(res.errors),
+                )
             self._b = []
-
-
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     conf = config[DOMAIN]
 
-
     dd_conf = Configuration()
 
-    tags = conf["tags"].split(",")
+    default_tags = conf["tags"].split(",")
     prefix = conf["prefix"]
     flush_period_sec = conf["flush_period_sec"]
 
@@ -138,26 +149,33 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
         if state is None or state.state == STATE_UNKNOWN:
             return
 
-        states = dict(state.attributes)
-        metric = f"{prefix}.{state.domain}"
-        event_tags = [f"entity:{state.entity_id}"] + tags
+        attrs = dict(state.attributes)
+        metric = f"{prefix}.{state.entity_id}"
+        device_class = attrs.get("device_class", "unknown")
+        state_class = attrs.get("state_class", "unknown")
+        tags = [
+            f"domain:{state.domain}",
+            f"device_class:{device_class}",
+            f"state_class:{state_class}",
+        ] + default_tags
+        unit = attrs.get("unit_of_measurement", "")
 
-        for key, value in states.items():
+        for key, value in attrs.items():
             if isinstance(value, (float, int)):
                 attribute = f"{metric}.{key.replace(' ', '_')}"
                 value = int(value) if isinstance(value, bool) else value
 
-                m_id = MetricId(attribute, tuple(event_tags))
+                m_id = MetricId(attribute, tuple(tags), "")
                 buffer.buffer_or_send(Value(m_id, ts(), value))
-                _LOGGER.debug("Sent metric %s: %s (tags: %s)", attribute, value, event_tags)
+                _LOGGER.debug("Sent metric %s: %s (tags: %s)", attribute, value, tags)
 
         try:
             value = state_helper.state_as_number(state)
         except ValueError:
-            _LOGGER.error("Error sending %s: %s (tags: %s)", metric, state.state, event_tags)
+            _LOGGER.error("Error sending %s: %s (tags: %s)", metric, state.state, tags)
             return
 
-        m_id = MetricId(metric, tuple(tags))
+        m_id = MetricId(metric, tuple(tags), unit)
         buffer.buffer_or_send(Value(m_id, ts(), value))
 
         _LOGGER.debug("Sent metric %s: %s (tags: %s)", metric, value, tags)
